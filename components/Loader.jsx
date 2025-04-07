4fa7bb160d4e590a6b409751d7ffdbd2dd6df30d
gottajsx <gottajsx@gmail.com>
2025-04-07 21:02:21 +0200

    Transform Loader.jsx with tailwind

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,11 +1,9 @@
-import "@styles/Loader.scss"
-
 const Loader = () => {
   return (
-    <div className="loader">
-      <div className="loader-inner"></div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-16 h-16 border-4 border-blue-500 border-solid border-t-transparent rounded-full animate-spin"></div>
     </div>
-  )
-}
+  );
+};
 
 export default Loader;
