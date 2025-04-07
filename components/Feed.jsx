bba367ca626126840e3266dfa1a1af547f1cf936
gottajsx <gottajsx@gmail.com>
2025-04-07 20:57:03 +0200

    Transform Feed.jsx with tailwind

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,16 +3,13 @@
 import { categories } from "@data";
 import WorkList from "./WorkList";
 import { useEffect, useState } from "react";
-import "@styles/Categories.scss";
 import Loader from "./Loader";
 
 const Feed = () => {
   const [loading, setLoading] = useState(true);
-
   const [selectedCategory, setSelectedCategory] = useState("All");
-
   const [workList, setWorkList] = useState([]);
-  
+
   const getWorkList = async () => {
     const response = await fetch(`/api/work/list/${selectedCategory}`);
     const data = await response.json();
@@ -28,12 +25,14 @@ const Feed = () => {
     <Loader />
   ) : (
     <>
-      <div className="categories">
+      <div className="flex justify-center items-start flex-wrap gap-5 my-5">
         {categories?.map((item, index) => (
           <p
-            onClick={() => setSelectedCategory(item)}
-            className={`${item === selectedCategory ? "selected" : ""}`}
             key={index}
+            onClick={() => setSelectedCategory(item)}
+            className={`text-sm cursor-pointer transition duration-300 ease-in-out hover:opacity-80 ${
+              item === selectedCategory ? "font-semibold text-blue-500" : ""
+            }`}
           >
             {item}
           </p>
