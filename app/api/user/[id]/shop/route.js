0d84a772c34a90b05ae933d806cc4409b1bf63d2
gottajsx <gottajsx@gmail.com>
2025-04-09 20:45:28 +0200

    Modify shop route for nextjs 15

--- app/api/user/[id]/shop/route.js
+++ app/api/user/[id]/shop/route.js
@@ -1,20 +1,22 @@
 import Work from "@models/Work";
 import User from "@models/User";
-
 import { connectToDB } from "@mongodb/database";
 
-export const GET = async (req, { params }) => {
+export const GET = async (req, context) => {
   try {
     await connectToDB();
 
-    const user = await User.findById(params.id);
-    const workList = await Work.find({ creator: params.id }).populate("creator");
+    const { params } = context;
+    const { id } = await params; // Résolution de 'params' avec 'await'
+
+    const user = await User.findById(id);
+    const workList = await Work.find({ creator: id }).populate("creator");
 
     user.works = workList;
     await user.save();
 
-    return new Response(JSON.stringify({ user: user, workList: workList }), { status: 200 });
+    return new Response(JSON.stringify({ user, workList }), { status: 200 });
   } catch (err) {
-    return new Response("Failed to fetch work list by user", { status: 500 })
+    return new Response("Failed to fetch work list by user", { status: 500 });
   }
-}
+};
